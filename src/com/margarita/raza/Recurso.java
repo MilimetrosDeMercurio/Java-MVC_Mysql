3b77638c64d801bfa99ca4c284e090c237eedcbb
Margarita <00225117@uca.edu.sv>
2018-06-11 08:39:30 -0600

    Recurso, se encuentra el nombre del jugador con la cantidad que tiene

---
+++ src/com/margarita/raza/Recurso.java
@@ -0,0 +1,31 @@
+
+package com.margarita.raza;
+
+/**
+ *
+ * @author Margarita
+ */
+public class Recurso {
+    private String nombre;
+    private int cant;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCant() {
+        return cant;
+    }
+
+    public void setCant(int cant) {
+        
+        
+        this.cant = cant;
+    }
+    
+    
+}
