6e4440b36e7bdb8cca1a747b171d97258ab945cb
Margarita <00225117@uca.edu.sv>
2018-06-11 08:38:03 -0600

    Recolector, donde se encuentra el estado de vida de ambos jugadores

---
+++ src/com/margarita/raza/Recolector.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.margarita.raza;
+
+//import java.util.ArrayList;
+
+/**
+ *
+ * @author Margarita
+ */
+public class Recolector<R> extends Edificacion {
+    
+//ATRIBUTOS
+    R Recurso;
+    
+//CONSTRUCTOR POR DEFECTO
+    
+    public Recolector(int vida) {
+        super(vida);
+    }
+
+    public R getRecurso() {
+        return Recurso;
+    }
+
+    public void setRecurso(R Recurso) {
+        this.Recurso = Recurso;
+    }
+
+    
+            
+            
+    
+}
