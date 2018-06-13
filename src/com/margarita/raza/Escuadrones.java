ba532880b293f191e72fe63beab87ba8f946dd0f
Margarita <00225117@uca.edu.sv>
2018-06-13 16:34:09 -0600

    Escuadrones, palntilla de ellos

---
+++ src/com/margarita/raza/Escuadrones.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.margarita.raza;
+
+/**
+ *
+ * @author Margarita
+ */
+class Escuadrones {
+    
+    String nombre;
+    int vida;
+
+    public Escuadrones(String nombre, int vida) {
+        this.nombre = nombre;
+        this.vida = vida;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getVida() {
+        return vida;
+    }
+
+    public void setVida(int vida) {
+        this.vida = vida;
+    }
+    
+    
+}
